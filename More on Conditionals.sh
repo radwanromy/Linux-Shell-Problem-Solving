6a9dadd41d5264ae5ce9daebd22c34c6faadfda8
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-06-17 14:55:29 +0600

    Commit Shell 8

---
+++ More on Conditionals.sh
@@ -0,0 +1,12 @@
+read x;
+read y;
+read z;
+if [ $x -eq $y ] && [ $y -eq $z ]
+then
+echo "EQUILATERAL";
+elif [ $x -ne $y ] && [ $x -ne $z ] && [ $y -ne $z ]
+then
+echo "SCALENE";
+else
+echo "ISOSCELES";
+fi
